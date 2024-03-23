b1361a363cd5a14455656b7055ffe3f8c960a166
plqew <plqew@DESKTOP-C3Q2D24>
2024-03-23 17:54:43 +0200

    Добавьте файлы проекта.

---
+++ lab9(2)_Петрушев.cpp/lab9(2)_Петрушев.cpp.cpp
@@ -0,0 +1,47 @@
+﻿
+
+
+
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int n = 3;
+    int table[n][n] = { {1, -2, 3}, {4, 5, -6}, {7, 8, 9} };
+
+    int sumGoodRows = 0;
+    int sumBadDiagonal = 0;
+    int countBadDiagonal = 0;
+
+    for (int i = 0; i < n; i++) {
+        int rowSum = 0;
+        bool badNumbers = false;
+        for (int j = 0; j < n; j++) {
+            if (table[i][j] < 0) {
+                badNumbers = true;
+                if (i == j) {
+                    sumBadDiagonal += table[i][j];
+                    countBadDiagonal++;
+                }
+            }
+            else {
+                rowSum += table[i][j];
+            }
+        }
+        if (!badNumbers)
+            sumGoodRows += rowSum;
+    }
+
+    double avgBadDiagonal = 0;
+    if (countBadDiagonal > 0)
+        avgBadDiagonal = (double)sumBadDiagonal / countBadDiagonal;
+
+    cout << "Сума чисел в хороших рядках: " << sumGoodRows << endl;
+    cout << "Середнє поганих чисел на діагоналі: " << avgBadDiagonal << endl;
+
+    return 0;
+}
